950df9bd51b79d575d6b04b1bd2ae683786464dd
Maksos <94627655+puskainrestpease@users.noreply.github.com>
2025-05-06 17:34:41 +0300

    Update Sound.kt

--- src/main/java/com/example/picosounpad/Sound.kt
+++ src/main/java/com/example/picosounpad/Sound.kt
@@ -1,11 +1,12 @@
-package com.example.picosoundpad
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "sounds")
-data class Sound(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val name: String,
-    val filePath: String
+package com.example.picosounpad  
+  
+import androidx.room.Entity  
+import androidx.room.PrimaryKey  
+  
+@Entity(tableName = "sounds")  
+data class Sound(  
+    @PrimaryKey(autoGenerate = true)  
+    val id: Int = 0,  
+    val name: String,  
+    val filePath: String  
 )
