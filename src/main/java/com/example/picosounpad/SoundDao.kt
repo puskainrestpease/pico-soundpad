7db1fe584a8876a774273b92edfbdcc1ad70966f
Maksos <94627655+puskainrestpease@users.noreply.github.com>
2025-05-06 17:34:55 +0300

    Update SoundDao.kt

--- src/main/java/com/example/picosounpad/SoundDao.kt
+++ src/main/java/com/example/picosounpad/SoundDao.kt
@@ -1,18 +1,21 @@
-package com.example.picosoundpad
-
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-
-@Dao
-interface SoundDao {
-
-    @Insert
-    suspend fun insert(sound: Sound)
-
-    @Query("SELECT * FROM sounds")
-    suspend fun getAllSounds(): List<Sound>
-
-    @Query("DELETE FROM sounds WHERE id = :id")
-    suspend fun deleteById(id: Int)
+package com.example.picosounpad  
+  
+import androidx.room.Dao  
+import androidx.room.Insert  
+import androidx.room.OnConflictStrategy  
+import androidx.room.Query  
+  
+@Dao  
+interface SoundDao {  
+    @Insert(onConflict = OnConflictStrategy.REPLACE)  
+    suspend fun insert(sound: Sound)  
+      
+    @Query("SELECT * FROM sounds")  
+    suspend fun getAllSounds(): List<Sound>  
+      
+    @Query("SELECT * FROM sounds WHERE id = :id")  
+    suspend fun getSoundById(id: Int): Sound?  
+      
+    @Query("DELETE FROM sounds WHERE id = :id")  
+    suspend fun deleteSound(id: Int)  
 }
